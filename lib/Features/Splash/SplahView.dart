c977e37b137059fa0e39b928998d913a0999330f
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2023-10-21 15:10:42 +0300

    splshViewIntro

---
+++ lib/Features/Splash/SplahView.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_2/Features/Splash/widgets/SplashViewBody.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
