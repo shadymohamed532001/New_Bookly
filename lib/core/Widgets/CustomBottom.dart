4f10ccc578025e12ceb06d57ebd1a11191fbdc48
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2023-10-23 17:58:20 +0300

    Create Items Grip UI

---
+++ lib/core/Widgets/CustomBottom.dart
@@ -0,0 +1,39 @@
+import 'package:bookly_2/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({
+    super.key,
+    required this.onPressed,
+    required this.bottomTittle,
+    this.height = 60,
+    this.width = double.infinity,
+    this.textStyle,
+  });
+
+  final void Function() onPressed;
+  final String bottomTittle;
+  final double? height;
+  final TextStyle? textStyle;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: height,
+        width: width,
+        child: ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0XFFDE7773),
+              shadowColor: Colors.grey,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+            child: Text(
+              bottomTittle,
+              style: textStyle,
+            )));
+  }
+}
